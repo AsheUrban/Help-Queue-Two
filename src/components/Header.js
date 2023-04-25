089228aa17f685a226f907e8fef005d7b477ba53
Ashe Urban <ashe@goldentongue.com>
2023-04-25 10:34:36 -0700

    Follow along with lessons for adding react with redux.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import ticketsImage from "./../img/tickets-image.png";
 function Header(){
   return (
     <React.Fragment>
-      <img src={ticketsImage} alt="An image of two tickets" />
+      <img src={ticketsImage} alt="Two paper ticket stubs" />
     </React.Fragment>
   );
 }
