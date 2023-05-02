8371db46407f609c9fcf3a1c2042d8c85f7887c8
Ashe Urban <ashe@goldentongue.com>
2023-05-02 14:40:01 -0700

    Revert "PASS all tests for combined reducers."
    This reverts commit d476c56534e11e4f88e760b9306db729a36fe9c7.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -7,6 +7,26 @@ let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
+// test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+//   const action = {
+//     type: 'ADD_TICKET',
+//     names: 'Ryan & Aimen',
+//     location: '4b',
+//     issue: 'Redux action is not working correctly.',
+//     id: 1
+//   }
+//   store.dispatch(action);
+//   expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
+// });
+
+// test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+//   const action = {
+//     type: 'TOGGLE_FORM'
+//   }
+//   store.dispatch(action);
+//   expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+// });
+
 test('Check that initial state of ticketListReducer matches root reducer', () => {
   expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, { type: null }));
 });
@@ -15,31 +35,11 @@ test('Check that initial state of formVisibleReducer matches root reducer', () =
   expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
 });
 
-test('Should return default state if no action type is recognized', () => {
-  expect(rootReducer({}, { type: null })).toEqual({
-    mainTicketList: {},
-    formVisibleOnPage: false
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      mainTicketList: {},
+      formVisibleOnPage: false
+    });
   });
-});
-
-test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
-  const action = {
-    type: 'ADD_TICKET',
-    names: 'Ryan & Aimen',
-    location: '4b',
-    issue: 'Redux action is not working correctly.',
-    id: 1
-  }
-  store.dispatch(action);
-  expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
-});
-
-test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
-  const action = {
-    type: 'TOGGLE_FORM'
-  }
-  store.dispatch(action);
-  expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
-});
 
 });
